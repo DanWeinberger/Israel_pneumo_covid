3d5f475dcc083c2f2ed9bc9617fc9fcfcb5d6de4
weinbergerlab <dweinber@gmail.com>
2022-03-25 15:36:27 -0400

    works2

--- R/gen_pred_interval_inla_ridge_ar1.R
+++ R/gen_pred_interval_inla_ridge_ar1.R
@@ -95,15 +95,4 @@ gen_pred_interval_inla_ridge_ar1 <- function(X1,Zb,inla_obj, Za,  covar.df, outc
 }
 
 
-# 
-# 
-# pred.interval.func <- function(sample.ds,  dist=c('nbinomial','poisson')){
-#   mu1 <- exp(sample.ds$latent[grep('Predictor', row.names(sample.ds$latent))])
-#   if(dist=='nbinomial'){
-#     nb.size1 = sample.ds$hyperpar
-#     pred <- replicate(10, rnbinom(n=length(mu1), mu=mu1, size=nb.size1), simplify = 'array')
-#   }else{
-#     pred <- replicate(10, rpois(n=length(mu1), lambda=mu1), simplify = 'array')
-#   }
-#   return(pred)
-# }
+
